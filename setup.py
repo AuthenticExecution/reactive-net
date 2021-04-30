053959e2485690389bf4c604ad6d3b74d0ce845b
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-04-30 18:35:04 +0200

    upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="reactive-net",
-    version="0.1.1",
+    version="0.2",
     author="Gianluca Scopelliti",
     author_email="gianlu.1033@gmail.com",
     description="Networking library for reactive-tools",
