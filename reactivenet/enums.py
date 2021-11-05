0e99987fc00e6099e9101e56ce78acae884239bf
Gianluca Scopelliti <gianlu.1033@gmail.com>
2021-11-05 15:03:20 +0000

    improve formatting, add CI

--- reactivenet/enums.py
+++ reactivenet/enums.py
@@ -1,15 +1,15 @@
 from enum import IntEnum
 
-# Reactive Command codes
+
 class ReactiveCommand(IntEnum):
-    Connect             = 0x0
-    Call                = 0x1
-    RemoteOutput        = 0x2
-    Load                = 0x3
-    Ping                = 0x4
-    RegisterEntrypoint  = 0x5
-    Output              = 0x6 # called by software modules in SGX and Native
-    RemoteRequest       = 0x7
+    Connect = 0x0
+    Call = 0x1
+    RemoteOutput = 0x2
+    Load = 0x3
+    Ping = 0x4
+    RegisterEntrypoint = 0x5
+    Output = 0x6  # called by software modules in SGX and Native
+    RemoteRequest = 0x7
 
     def has_response(self):
         if self == ReactiveCommand.RemoteOutput:
@@ -20,21 +20,19 @@ class ReactiveCommand(IntEnum):
         return True
 
 
-# Reactive Result codes
 class ReactiveResult(IntEnum):
-    Ok                  = 0x0
-    IllegalCommand      = 0x1
-    IllegalPayload      = 0x2
-    InternalError       = 0x3
-    BadRequest          = 0x4
-    CryptoError         = 0x5
-    NotAttestedYet      = 0x6
-    GenericError        = 0x7
+    Ok = 0x0
+    IllegalCommand = 0x1
+    IllegalPayload = 0x2
+    InternalError = 0x3
+    BadRequest = 0x4
+    CryptoError = 0x5
+    NotAttestedYet = 0x6
+    GenericError = 0x7
 
 
-# Reactive Entrypoint ids
 class ReactiveEntrypoint(IntEnum):
-    SetKey              = 0x0
-    Attest              = 0x1
-    HandleInput         = 0x2
-    HandleHandler       = 0x3
+    SetKey = 0x0
+    Attest = 0x1
+    HandleInput = 0x2
+    HandleHandler = 0x3
