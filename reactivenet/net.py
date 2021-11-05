1bd5650fe599fed48869c0b867765de28e18bc9e
gianlu33 <36645341+gianlu33@users.noreply.github.com>
2021-11-05 16:04:29 +0100

    improve formatting, add CI (#1)

--- reactivenet/net.py
+++ reactivenet/net.py
@@ -3,28 +3,35 @@ import ipaddress
 import asyncio
 import contextlib
 
-from .enums import *
+from .enums import ReactiveCommand, ReactiveResult
+
 
 class Error(Exception):
     pass
 
 
-# Message: format <size u16><payload>
 class Message():
+    """
+    Message: format <size u16><payload>
+    """
+
     def __init__(self, payload=bytearray()):
         self.payload = payload
 
-
-    # form the byte array according to the format
     def pack(self):
+        """
+        form the byte array according to the format
+        """
         size = struct.pack('!H', len(self.payload))
 
         return size + self.payload
 
-
-    # read from an asyncio StreamReader
     @staticmethod
     async def read(reader):
+        """
+        read from an asyncio StreamReader
+        """
+
         # read len
         size = await reader.readexactly(2)
         size = struct.unpack('!H', size)[0]
@@ -37,28 +44,35 @@ class Message():
         return Message(payload)
 
 
-# ResultMessage: format <code u8><size u16><payload>
 class ResultMessage():
+    """
+    ResultMessage: format <code u8><size u16><payload>
+    """
+
     def __init__(self, code, message):
         self.code = code
         self.message = message
 
-
-    # form the byte array according to the format
     def pack(self):
+        """
+        form the byte array according to the format
+        """
         code = struct.pack('!B', self.code)
 
         return code + self.message.pack()
 
-
-    # check if the command succeeded
     def ok(self):
+        """
+        check if the command succeeded
+        """
         return self.code == ReactiveResult.Ok
 
-
-    # read from an asyncio StreamReader
     @staticmethod
     async def read(reader):
+        """
+        read from an asyncio StreamReader
+        """
+
         # read result code
         code = await reader.readexactly(1)
         code = struct.unpack('!B', code)[0]
@@ -73,66 +87,80 @@ class ResultMessage():
         return ResultMessage(code, message)
 
 
-# CommandMessage: format <code u8><size u16><payload>
 class CommandMessage():
+    """
+    CommandMessage: format <code u8><size u16><payload>
+    """
+
     def __init__(self, code, message, ip=None, port=None):
         self.code = code
         self.message = message
         self.__ip = ip
         self.__port = port
 
-
-    # get destination IP
-    # raises exception if the IP is not specified
     @property
     def ip(self):
+        """
+        get destination IP
+        raises exception if the IP is not specified
+        """
         if self.__ip is None:
             raise Error("IP address not specified")
 
         return self.__ip
 
-
-    # get destination port
-    # raises exception if the port is not specified
     @property
     def port(self):
+        """
+        get destination port
+        raises exception if the port is not specified
+        """
+
         if self.__port is None:
             raise Error("TCP port not specified")
 
         return self.__port
 
-
-    # form the byte array according to the format
     def pack(self):
+        """
+        form the byte array according to the format
+        """
+
         code = struct.pack('!B', self.code)
 
         return code + self.message.pack()
 
-
-    # set destination ip and port
     def set_dest(self, ip, port):
+        """
+        set destination ip and port
+        """
+
         self.__ip = ip
         self.__port = port
 
-
-    # check if the command will have a response
     def has_response(self):
+        """
+        check if the command will have a response
+        """
         return self.code.has_response()
 
-
-    # send the command to the destination IP and port
     async def send(self):
-        reader, writer = await asyncio.open_connection(str(self.ip), self.port)
+        """
+        send the command to the destination IP and port
+        """
+        _, writer = await asyncio.open_connection(str(self.ip), self.port)
 
         with contextlib.closing(writer):
             writer.write(self.pack())
             await writer.drain()
 
-
-    # send the command to the destination IP and port
-    # also wait for the response
-    # raises exception if the command does not have a response
     async def send_wait(self):
+        """
+        send the command to the destination IP and port
+        also wait for the response
+        raises exception if the command does not have a response
+        """
+
         if not self.has_response():
             raise Error("This command has not response: call send() instead")
 
@@ -143,10 +171,12 @@ class CommandMessage():
             await writer.drain()
             return await ResultMessage.read(reader)
 
-
-    # read from an asyncio StreamReader
     @staticmethod
     async def read(reader):
+        """
+        read from an asyncio StreamReader
+        """
+
         # read command code
         code = await reader.readexactly(1)
         code = struct.unpack('!B', code)[0]
@@ -160,11 +190,12 @@ class CommandMessage():
 
         return CommandMessage(code, message)
 
-
-    # read from an asyncio StreamReader
-    # also read ip and port from reader, that are sent before the command
     @staticmethod
     async def read_with_ip(reader):
+        """
+        read from an asyncio StreamReader
+        also read ip and port from reader, that are sent before the command
+        """
         ip = await reader.readexactly(4)
         ip = struct.unpack('!I', ip)[0]
         ip = ipaddress.ip_address(ip)
@@ -178,18 +209,22 @@ class CommandMessage():
         return cmd
 
 
-# CommandMessageLoad: since the loading process is different for each
-#                     architecture, this class receives the payload already
-#                     formed by the caller. The code is fixed: ReactiveCommand.Load
-# Inherited: has_response(), send() and send_wait() from CommandMessage
 class CommandMessageLoad(CommandMessage):
+    """
+    CommandMessageLoad: since the loading process is different for each
+                        architecture, this class receives the payload already
+                        formed by the caller. The code is fixed: ReactiveCommand.Load
+    Inherited: has_response(), send() and send_wait() from CommandMessage
+    """
+
     def __init__(self, payload, ip, port):
         super().__init__(ReactiveCommand.Load, None, ip, port)
         self.payload = payload
 
-
-    # form the byte array according to the format
     def pack(self):
+        """
+        form the byte array according to the format
+        """
         code = struct.pack('!B', self.code)
 
         return code + self.payload
